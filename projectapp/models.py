4c3a26e46ea945fdec30cd79db312b33aaca0ab9
humblebee <colock123@gmail.com>
2022-01-28 11:46:45 +0900

    django 40 bug fixed

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Project(models.Model):
-    image = models.ImageField(upload_to='project/',null=False)
+    image = models.ImageField(upload_to='project/', null=False)
     #image를 올리면 media/project/ 에 upload됨
-    title = models.CharField(max_length=25,null=False)
-    description = models.CharField(max_length =25,null = True)
-    created_at = models.DateTimeField(auto_created=True)
+    title = models.CharField(max_length=25, null=False)
+    description = models.CharField(max_length =25, null = True)
+    created_at = models.DateTimeField(auto_now=True)

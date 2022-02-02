6925f7753b833908701d63d71c3c0bae3af663ba
humblebee <colock123@gmail.com>
2022-02-02 23:51:00 +0900

    fixed

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
--- pragmatic/settings/local.py
+++ pragmatic/settings/local.py
@@ -26,3 +26,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+    }
+}
+
